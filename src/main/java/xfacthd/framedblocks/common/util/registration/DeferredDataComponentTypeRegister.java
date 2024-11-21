1ea037e7a29e3d41868cb492df01c33476565f92
XFactHD <xfacthd@gmx.de>
2024-11-21 03:28:11 +0100

    Switch to non-deprecated DeferredRegister.DataComponents constructor

--- src/main/java/xfacthd/framedblocks/common/util/registration/DeferredDataComponentTypeRegister.java
+++ src/main/java/xfacthd/framedblocks/common/util/registration/DeferredDataComponentTypeRegister.java
@@ -2,6 +2,7 @@ package xfacthd.framedblocks.common.util.registration;
 
 import net.minecraft.core.Registry;
 import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -14,7 +15,7 @@ public final class DeferredDataComponentTypeRegister extends DeferredRegister.Da
 {
     private DeferredDataComponentTypeRegister(String namespace)
     {
-        super(namespace);
+        super(Registries.DATA_COMPONENT_TYPE, namespace);
     }
 
     @Override
