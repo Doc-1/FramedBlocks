a32c56ad3e6e9fe1c4d57decee7b155dc52c33e8
XFactHD <xfacthd@gmx.de>
2025-01-31 03:05:27 +0100

    Slightly simplify datagen setup

--- src/main/java/xfacthd/framedblocks/common/datagen/GeneratorHandler.java
+++ src/main/java/xfacthd/framedblocks/common/datagen/GeneratorHandler.java
@@ -1,13 +1,9 @@
 package xfacthd.framedblocks.common.datagen;
 
-import net.minecraft.core.HolderLookup;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.PackOutput;
 import net.minecraft.world.item.Item;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 import net.neoforged.neoforge.data.loading.DatagenModLoader;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -17,8 +13,6 @@ import xfacthd.framedblocks.api.util.FramedConstants;
 import xfacthd.framedblocks.client.itemmodel.DynamicItemTintProviders;
 import xfacthd.framedblocks.common.compat.ae2.AppliedEnergisticsCompat;
 import xfacthd.framedblocks.common.datagen.providers.*;
-
-import java.util.concurrent.CompletableFuture;
 
 @Mod(value = FramedConstants.MOD_ID, dist = Dist.CLIENT)
 @SuppressWarnings("UtilityClassWithPublicConstructor")
@@ -41,23 +35,17 @@ public final class GeneratorHandler
 
     private static void onGatherData(final GatherDataEvent.Client event)
     {
-        DataGenerator gen = event.getGenerator();
-        PackOutput output = gen.getPackOutput();
-        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
-
         // TODO: needs a better solution that works when other mods generate data
         DynamicItemTintProviders.init();
 
-        gen.addProvider(true, new FramedSpriteSourceProvider(output, lookupProvider));
-        gen.addProvider(true, new FramedBlockModelProvider(output));
-        gen.addProvider(true, new FramedItemModelProvider(output));
-        gen.addProvider(true, new FramedLanguageProvider(output));
+        event.createProvider(FramedSpriteSourceProvider::new);
+        event.createProvider(FramedBlockModelProvider::new);
+        event.createProvider(FramedItemModelProvider::new);
+        event.createProvider(FramedLanguageProvider::new);
 
-        gen.addProvider(true, new FramedLootTableProvider(output, lookupProvider));
-        gen.addProvider(true, new FramedRecipeProvider.Runner(output, lookupProvider));
-        gen.addProvider(true, new FramingSawRecipeProvider.Runner(output, lookupProvider));
-        BlockTagsProvider tagProvider = new FramedBlockTagProvider(output, lookupProvider);
-        gen.addProvider(true, tagProvider);
-        gen.addProvider(true, new FramedItemTagProvider(output, lookupProvider, tagProvider.contentsGetter()));
+        event.createProvider(FramedLootTableProvider::new);
+        event.createProvider(FramedRecipeProvider.Runner::new);
+        event.createProvider(FramingSawRecipeProvider.Runner::new);
+        event.createBlockAndItemTags(FramedBlockTagProvider::new, FramedItemTagProvider::new);
     }
 }
