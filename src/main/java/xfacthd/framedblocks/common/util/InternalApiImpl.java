ffda48aa979d66516889e1e937f70b7e3a375e3f
XFactHD <xfacthd@gmx.de>
2025-06-23 03:37:05 +0200

    Try protecting against possible deadlock when getting light emission, see #632

--- src/main/java/xfacthd/framedblocks/common/util/InternalApiImpl.java
+++ src/main/java/xfacthd/framedblocks/common/util/InternalApiImpl.java
@@ -1,9 +1,16 @@
 package xfacthd.framedblocks.common.util;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.SectionPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.chunk.ChunkSource;
+import net.minecraft.world.level.chunk.ImposterProtoChunk;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.world.level.chunk.LightChunk;
+import org.jetbrains.annotations.Nullable;
 import xfacthd.framedblocks.api.internal.InternalAPI;
 import xfacthd.framedblocks.common.FBContent;
 import xfacthd.framedblocks.common.compat.flywheel.FlywheelCompat;
@@ -12,13 +19,14 @@ import xfacthd.framedblocks.common.compat.starlight.StarlightCompat;
 public final class InternalApiImpl implements InternalAPI
 {
     @Override
+    @Nullable
     public BlockEntity getExistingBlockEntity(BlockGetter level, BlockPos pos)
     {
         if (FlywheelCompat.isVirtualLevel(level))
         {
             return level.getBlockEntity(pos);
         }
-        return level.getExistingBlockEntity(pos);
+        return getExistingBlockEntity0(level, pos);
     }
 
     @Override
@@ -40,5 +48,36 @@ public final class InternalApiImpl implements InternalAPI
             camoTag.put("state", stateTag);
             tag.put(camoKey, camoTag);
         }
+    }
+
+    @Nullable
+    private static BlockEntity getExistingBlockEntity0(BlockGetter blockGetter, BlockPos pos)
+    {
+        if (blockGetter instanceof Level level)
+        {
+            int chunkX = SectionPos.blockToSectionCoord(pos.getX());
+            int chunkZ = SectionPos.blockToSectionCoord(pos.getZ());
+            ChunkSource chunkSource;
+            try
+            {
+                chunkSource = level.getChunkSource();
+            }
+            catch (Throwable t)
+            {
+                // Some mods' fake levels think they are funny for throwing in getChunkSource()
+                return null;
+            }
+            LightChunk chunk = chunkSource.getChunkForLighting(chunkX, chunkZ);
+            return chunk != null ? getExistingBlockEntity0(chunk, pos) : null;
+        }
+        else if (blockGetter instanceof LevelChunk chunk)
+        {
+            return chunk.getBlockEntities().get(pos);
+        }
+        else if (blockGetter instanceof ImposterProtoChunk chunk)
+        {
+            return getExistingBlockEntity0(chunk.getWrapped(), pos);
+        }
+        return blockGetter.getBlockEntity(pos);
     }
 }
