b2529da51eec50b00c056c633a28d354ceaf7a81
XFactHD <xfacthd@gmx.de>
2025-11-18 08:12:40 +0100

    Fix double blocks breaking instantly

--- src/main/java/io/github/xfacthd/framedblocks/common/block/FramedDoubleBlock.java
+++ src/main/java/io/github/xfacthd/framedblocks/common/block/FramedDoubleBlock.java
@@ -1,15 +1,23 @@
 package io.github.xfacthd.framedblocks.common.block;
 
 import io.github.xfacthd.framedblocks.api.block.AbstractFramedDoubleBlock;
+import io.github.xfacthd.framedblocks.api.block.IFramedBlock;
 import io.github.xfacthd.framedblocks.common.data.BlockType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.pathfinder.PathComputationType;
+
+import java.util.function.UnaryOperator;
 
 public abstract class FramedDoubleBlock extends AbstractFramedDoubleBlock implements IFramedDoubleBlockInternal
 {
     public FramedDoubleBlock(BlockType blockType, Properties props)
     {
-        super(blockType, props);
+        this(blockType, props, UnaryOperator.identity());
+    }
+
+    public FramedDoubleBlock(BlockType blockType, Properties props, UnaryOperator<Properties> propertyModifier)
+    {
+        super(blockType, propertyModifier.apply(IFramedBlock.applyDefaultProperties(props, blockType)));
     }
 
     @Override
